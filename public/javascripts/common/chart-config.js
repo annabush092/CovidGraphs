6a09dd1ae39df756db9a6bb565723c93eafb6093
Anna Bush <annabush092@gmail.com>
2020-07-01 14:05:18 -0400

    graph formatting

--- public/javascripts/common/chart-config.js
+++ public/javascripts/common/chart-config.js
@@ -1,22 +1,69 @@
 const _colorArr = [
-    'rgba(0, 188, 241, 1)', // Southern Light Blue
-    'rgba(178, 210, 53, 1)', // Southern Light Green
-    'rgba(11, 59, 93, 1)', // Southern Dark Blue
+    'rgb(240,163,255)', 
+    'rgb(0,117,220)', 
+    'rgb(153,63,0)', 
+    'rgb(76,0,92)', 
+    'rgb(25,25,25)', 
+    'rgb(0,92,49)', 
+    'rgb(43,206,72)', 
+    'rgb(255,204,153)', 
+    'rgb(128,128,128)', 
+    'rgb(148,255,181)', 
+    'rgb(143,124,0)', 
+    'rgb(157,204,0)', 
+    'rgb(194,0,136)', 
+    'rgb(0,51,128)', 
+    'rgb(255,164,5)', 
+    'rgb(255,168,187)', 
+    'rgb(66,102,0)', 
+    'rgb(255,0,16)', 
+    'rgb(94,241,242)', 
+    'rgb(0,153,143)', 
+    'rgb(224,255,102)', 
+    'rgb(116,10,255)', 
+    'rgb(153,0,0)', 
+    'rgb(255,255,128)', 
+    'rgb(255,255,0)', 
+    'rgb(255,80,5)'
+]
 
-    'rgba(0, 181, 175, 1)', // Southern Teal
-    'rgba(0, 125, 185, 1)', // Southern Blue
-    'rgba(0, 116, 109, 1)', // Southern Medium Teal
-    'rgba(82, 151, 54, 1)', // Southern Medium Green
-];
+// const _colorArr = [
+//     'rgba(0, 188, 241, 1)', 
+//     'rgba(178, 210, 53, 1)', // Southern Light Green
+//     'rgba(11, 59, 93, 1)', // Southern Dark Blue
+
+//     'rgba(0, 181, 175, 1)', // Southern Teal
+//     'rgba(0, 125, 185, 1)', // Southern Blue
+//     'rgba(0, 116, 109, 1)', // Southern Medium Teal
+//     'rgba(82, 151, 54, 1)', // Southern Medium Green
+// ];
 const _translucentColors = [
-    'rgba(0, 188, 241, 0.5)', // Southern Light Blue
-    'rgba(178, 210, 53, 0.5)', // Southern Light Green
-    'rgba(11, 59, 93, 0.5)', // Southern Dark Blue
-
-    'rgba(0, 181, 175, 0.5)', // Southern Teal
-    'rgba(0, 125, 185, 0.5)', // Southern Blue
-    'rgba(0, 116, 109, 0.5)', // Southern Medium Teal
-    'rgba(82, 151, 54, 0.5)', // Southern Medium Green
+    'rgba(240,163,255,0.5)', 
+    'rgba(0,117,220,0.5)', 
+    'rgba(153,63,0,0.5)', 
+    'rgba(76,0,92,0.5)', 
+    'rgba(25,25,25,0.5)', 
+    'rgba(0,92,49,0.5)', 
+    'rgba(43,206,72,0.5)', 
+    'rgba(255,204,153,0.5)', 
+    'rgba(128,128,128,0.5)', 
+    'rgba(148,255,181,0.5)', 
+    'rgba(143,124,0,0.5)', 
+    'rgba(157,204,0,0.5)', 
+    'rgba(194,0,136,0.5)', 
+    'rgba(0,51,128,0.5)', 
+    'rgba(255,164,5,0.5)', 
+    'rgba(255,168,187,0.5)', 
+    'rgba(66,102,0,0.5)', 
+    'rgba(255,0,16,0.5)', 
+    'rgba(94,241,242,0.5)', 
+    'rgba(0,153,143,0.5)', 
+    'rgba(224,255,102,0.5)', 
+    'rgba(116,10,255,0.5)', 
+    'rgba(153,0,0,0.5)', 
+    'rgba(255,255,128,0.5)', 
+    'rgba(255,255,0,0.5)', 
+    'rgba(255,80,5,0.5)'
 ];
 const _labelColors = [
     "#BCBEC0", // light gray
@@ -26,8 +73,6 @@ const _labelColors = [
 // Default line plot
 // Default line plot
 function getDefaultLineOptions(yIsPrice, plotXunit) {
-    let yLabel = yIsPrice ? "Price ($)" : "Average Power (W)";
-
     return {
         responsive: true,
         maintainAspectRatio: false,
@@ -50,11 +95,14 @@ function getDefaultLineOptions(yIsPrice, plotXunit) {
                     fontFamily: "Arial",
                     fontColor: _labelColors[1],
                     beginAtZero: true,
-                    min: 0
+                    min: 0,
+                    callback: function(value, index, values) {
+                        return value.toLocaleString();
+                    }
                 },
                 scaleLabel: {
                     display: true,
-                    labelString: yLabel,
+                    labelString: "People",
                     fontSize: 13,
                     fontFamily: "Arial",
                     fontColor: _labelColors[1]
@@ -76,7 +124,7 @@ function getDefaultLineOptions(yIsPrice, plotXunit) {
                 title: function (tooltipItem, data) {
                     // format x coordinate on tooltip
                     let rawDate = tooltipItem[0].xLabel || '';
-                    let newDate = moment(rawDate, "YYYY-MM-DD HH:mm:ss").format('MM/DD/YYYY h:mm:ss a');
+                    let newDate = moment(rawDate, "YYYY-MM-DD HH:mm:ss").format('MM/DD/YYYY');
 
                     return newDate;
                 },
@@ -85,15 +133,9 @@ function getDefaultLineOptions(yIsPrice, plotXunit) {
                     let label = data.datasets[tooltipItem.datasetIndex].label || '';
 
                     if (label) {
-                        label += ': ';
+                        label += ': ' + tooltipItem.yLabel.toLocaleString();
                     }
-
-                    if (yIsPrice) {
-                        label += "$" + tooltipItem.yLabel;
-                    } else {
-                        label += tooltipItem.yLabel + ' W';
-                    }
-
+                
                     return label;
                 }
             }
@@ -119,12 +161,15 @@ function graphPlot(plotPoints, plot, chartCanvas, yIsPrice, plotXunit, iColor) {
 
         lineData.push({
             label: device,
-            lineTension: 0,
+            // lineTension: 0,
+            // showLine: true,
+            // spanGaps: true,
+            // fill: false,
             backgroundColor: _translucentColors[iColor],
             borderColor: _colorArr[iColor],
-            borderWidth: "1px",
+            borderWidth: 1.5,
             pointBackgroundColor: _colorArr[iColor],
-            pointRadius: 1.5,
+            pointRadius: 1.8,
             data: xyVals
         });
 
